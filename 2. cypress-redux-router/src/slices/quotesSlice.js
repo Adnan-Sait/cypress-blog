216039eb9f49fe704d83e19c743271049d876b8f
Adnan Sait <adnansait74@gmail.com>
2023-10-01 23:24:14 +0530

    #2: Added author & category state updates

--- 2. cypress-redux-router/src/slices/quotesSlice.js
+++ 2. cypress-redux-router/src/slices/quotesSlice.js
@@ -1,16 +1,16 @@
 /**
  * @typedef     {Object}    QuotesSource
- * @property    {Number}    id
- * @property    {String}    quote
- * @property    {String[]}  category
- * @property    {String}    author
+ * @property    {number}    id
+ * @property    {string}    quote
+ * @property    {string[]}  category
+ * @property    {string}    author
  */
 
 /**
- * @typedef     {Object}                    QuotesSlice
- * @property    {QuotesSource[]}            quotes
- * @property    {Map<String, Number[]>}     authors
- * @property    {Map<String, Number[]>}     categories
+ * @typedef     {Object}                        QuotesSlice
+ * @property    {QuotesSource[]}                quotes
+ * @property    {Object.<string, number[]>}     authors
+ * @property    {Object.<string, number[]>}     categories
  */
 
 const { createSlice } = require("@reduxjs/toolkit");
@@ -20,9 +20,45 @@ const { createSlice } = require("@reduxjs/toolkit");
  */
 export const quotesInitialState = {
   quotes: [],
-  authors: new Map(),
-  categories: new Map(),
+  authors: {},
+  categories: {},
 };
+
+/**
+ * Creates the author-contentId object and saves it in the state.
+ *
+ * @param {QuotesSlice} state
+ */
+function updateAuthorsState(state) {
+  state.authors = state.quotes.reduce((acc, value) => {
+    if (acc[value.author]) {
+      acc[value.author].push(value.id);
+    } else {
+      acc[value.author] = [value.id];
+    }
+
+    return acc;
+  }, {});
+}
+
+/**
+ * Creates the category-contentId object and saves it in the state.
+ *
+ * @param {QuotesSlice} state
+ */
+function updateCategoriesState(state) {
+  state.categories = state.quotes.reduce((acc, value) => {
+    value.category.forEach((item) => {
+      if (acc[item]) {
+        acc[item].push(value.id);
+      } else {
+        acc[item] = [value.id];
+      }
+    });
+
+    return acc;
+  }, {});
+}
 
 const quotesSlice = createSlice({
   name: "quotes",
@@ -32,8 +68,12 @@ const quotesSlice = createSlice({
       const { payload } = action;
 
       state.quotes = payload;
+      updateAuthorsState(state);
+      updateCategoriesState(state);
     },
   },
 });
+
+export const { saveQuotes } = quotesSlice.actions;
 
 export default quotesSlice.reducer;
