17e8efee2c2505cb86273d66cd51c0cc7841b2ca
Adnan Sait <adnansait74@gmail.com>
2023-10-02 23:38:32 +0530

    Added bootstrap

--- 2. cypress-redux-router/src/index.js
+++ 2. cypress-redux-router/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Main from "./Main";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
