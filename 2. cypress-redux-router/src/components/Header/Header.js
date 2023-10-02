76af5425a2d90309f67b9ce3843f04f12142f4ee
Adnan Sait <adnansait74@gmail.com>
2023-10-02 23:55:00 +0530

    Added logo

--- 2. cypress-redux-router/src/components/Header/Header.js
+++ 2. cypress-redux-router/src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
 import styles from "./Header.module.css";
-import { NavLink } from "react-router-dom";
+import { ReactComponent as ReactLogo } from "../../assets/logo.svg";
 
 function Header() {
   return (
     <header className={styles.header}>
-      <div className="container d-flex">
+      <div className={`container ${styles.headerContainer}`}>
+        <Link className={styles.logoWrapper} to="/">
+          <ReactLogo className={styles.reactLogo} />
+          <h2 className={styles.logoText}>Redux-Router</h2>
+        </Link>
         <nav className={styles.navbar}>
           <NavLink className={styles.navItem} to="/">
             Home
